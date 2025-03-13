5708bcecef0f7a8af6874e37a7ce245eefaaea78
Dinesh sharma <dineshsharmadev93@gmail.com>
2025-03-13 14:30:50 +0530

    update the change

--- src/components/ProductdetailMain/Productdetailsmain.jsx
+++ src/components/ProductdetailMain/Productdetailsmain.jsx
@@ -4,25 +4,28 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import Select from "react-select";
 import Modal from "react-bootstrap/Modal";
-import { useSelector ,useDispatch} from "react-redux";
-import {  useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
 import { Userverifyed } from "../../redux/slice/userAuthSlice/UserSlice";
-import { Add_review_Slice, Delete_Review, Getting_Review } from "../../redux/slice/adminproductSlice/adminproductSlice";
+import {
+  Add_review_Slice,
+  Delete_Review,
+  Getting_Review,
+} from "../../redux/slice/adminproductSlice/adminproductSlice";
 import { toast } from "react-toastify";
 
-const ProductDetailsMain = (SingleproductState) => {  
+const ProductDetailsMain = (SingleproductState) => {
+  const productdata = [SingleproductState];
+  // form review data collection
+  const [description, setDesc] = useState("");
+  const [rating, setRateing] = useState("");
+  const param = useParams();
+  const productid = param.id.toString();
 
-      const productdata = [SingleproductState];
-      // form review data collection
-      const [description , setDesc] = useState("") 
-      const [rating , setRateing] = useState("")
-      const param = useParams()
-      const productid = param.id.toString()
-  
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -36,81 +39,76 @@ const ProductDetailsMain = (SingleproductState) => {
   ];
 
   // verify user
-  const {LoggeduserData ,Loginuserdata, loading} = useSelector((state)=>state.userauth)
-  const {getReview_state} = useSelector((state)=>state.products)
-  const {DeletedReview} = useSelector((state)=>state.products)
-  
-  
-  
+  const { LoggeduserData, Loginuserdata, loading } = useSelector(
+    (state) => state.userauth
+  );
+  const { getReview_state } = useSelector((state) => state.products);
+  const { DeletedReview } = useSelector((state) => state.products);
+
   const dispatch = useDispatch();
   // const [username , setUsername] = useState("")
 
-     const productidObj = {
-          productid
+  const productidObj = {
+    productid,
+  };
+
+  const username = LoggeduserData[0]?.firstname;
+  useEffect(() => {
+    dispatch(Userverifyed());
+  }, []);
+
+  const handleselect = (e) => {
+    setRateing(e.value);
+  };
+
+  const data = {
+    username,
+    rating,
+    description,
+    productid,
+  };
+
+  const submitReview = (e) => {
+    e.preventDefault();
+    // Scroll position save karein
+    if (rating.length === 0 || description.length === 0) {
+      toast.error("All fields are required !");
+    } else {
+      if (LoggeduserData.length > 0) {
+        dispatch(Add_review_Slice(data)).then((res) => {
+          window.location.reload();
+        });
+      } else {
+        toast("please login for add review !");
       }
+    }
+  };
 
-    const username = LoggeduserData[0]?.firstname;
-    useEffect(()=>{
-       dispatch(Userverifyed())
-    },[])
+  // show rating for a product
+  const [showrating, setShowRating] = useState(0);
+  useEffect(() => {
+    let totalrating = 0;
 
-       
-     
-      const handleselect = (e) =>{
-          setRateing(e.value)
-      }
-      
-      const data = {
-         username , 
-         rating , 
-         description,
-         productid
-      }
+    getReview_state.map((ele, key) => {
+      totalrating = totalrating + parseInt(ele.rating);
+    });
+    setShowRating(Math.round(totalrating / getReview_state.length));
+  }, [getReview_state]);
 
-      const submitReview = (e) =>{
-         e.preventDefault()
-         // Scroll position save karein
-         if(rating.length === 0 || description.length === 0){
-            toast.error("All fields are required !")
-         }else{
-           if(LoggeduserData.length > 0){
-             dispatch(Add_review_Slice(data)).then((res)=>{
-               window.location.reload();
-             })
-           }else{
-              toast("please login for add review !")
-           }
-         }
-      }
-      
-      
-      
+  // Delet review
+  const deleteReview = (reviewid) => {
+    const data = {
+      reviewid,
+    };
+    dispatch(Delete_Review(data)).then((res) => {
+      toast("Deleted !");
+    });
+  };
 
-      // show rating for a product 
-      const [showrating , setShowRating] = useState(0)
-      useEffect(()=>{
-         let totalrating = 0 ; 
+  useEffect(() => {
+    dispatch(Getting_Review(productidObj));
+  }, [DeletedReview]);
 
-         getReview_state.map((ele,key)=>{
-            totalrating = totalrating+parseInt(ele.rating)
-         })
-           setShowRating(Math.round(totalrating / getReview_state.length))
-      },[getReview_state])
-      
-      // Delet review
-      const deleteReview = (reviewid) =>{
-          const data ={
-            reviewid
-          }
-        dispatch(Delete_Review(data)).then((res)=>{
-            toast("Deleted !")
-        })
-      }
-
-      useEffect(()=>{
-        dispatch(Getting_Review(productidObj))   
-      },[DeletedReview])
-       
   return (
     <>
       <section className="product-details">
@@ -122,32 +120,43 @@ const ProductDetailsMain = (SingleproductState) => {
             {productdata[0].SingleproductState?.map((item, indexs) => {
               return (
                 <>
-                  <Col md={6} key={indexs} className="product-details__image mb-4">
-                    <img src={`${item.productimage}`} alt="Nike Shoes" width={500} />
+                  <Col
+                    md={6}
+                    key={indexs}
+                    className="product-details__image mb-4"
+                  >
+                    <img
+                      src={`${item.productimage}`}
+                      alt="Nike Shoes"
+                      width={500}
+                    />
                   </Col>
                   <Col md={6} className="product-details__info">
-                    <h3 className="product-details__name">{item.productname}</h3>
+                    <h3 className="product-details__name">
+                      {item.productname}
+                    </h3>
                     <div className="product-details__ratings">
                       <span
                         className="product-details__stars"
                         style={{ color: "orange" }}
                       >
-                        {
-                           Array.from({length : showrating}).map((ele,keys)=>{
-                            return    <i className="fa-solid fa-star"></i>
-                          })
-                        }         
+                        {Array.from({ length: showrating }).map((ele, keys) => {
+                          return <i className="fa-solid fa-star"></i>;
+                        })}
                         {/* Empty Stars */}
-                           {Array.from({ length: 5 - showrating }).map((_, index) => (
-                             <i key={index + showrating} className="fa-regular fa-star"></i>
-                            ))}
+                        {Array.from({ length: 5 - showrating }).map(
+                          (_, index) => (
+                            <i
+                              key={index + showrating}
+                              className="fa-regular fa-star"
+                            ></i>
+                          )
+                        )}
                       </span>
                       <span
                         className="product-details__rating-count"
                         style={{ marginLeft: "5px" }}
-                      >
-                        
-                      </span>
+                      ></span>
                     </div>
                     <div className="product-details__price">
                       MRP: <span>{item.price}</span>
@@ -169,9 +178,7 @@ const ProductDetailsMain = (SingleproductState) => {
                     <h3 className="product-details__about-title">
                       About The Item
                     </h3>
-                    <p className="product-details__about">
-                       {item.description}
-                    </p>
+                    <p className="product-details__about">{item.description}</p>
                     <button className="product-details__add-to-cart ">
                       Add to Cart
                     </button>
@@ -183,119 +190,149 @@ const ProductDetailsMain = (SingleproductState) => {
         </Container>
       </section>
 
-      {/*Customer Review Section */}
+      {/*Customer Review Section */}        
       <section className="review-section mt-5">
         <Container>
           <div className="d-flex justify-content-between align-items-center">
             <div>
-              <h2>Customer Review</h2>
+              <h2>Customer Review</h2> 
             </div>
             <div className="review-button">
               <button onClick={handleShow}>Write Review</button>
 
               <Modal show={show} onHide={handleClose}>
-              <form action="">
-                <Modal.Header closeButton>
-                  <Modal.Title>Write Your Review</Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="modals-body">
-                  {
-                    LoggeduserData.map((userdata , index)=>{
-                      return(
+                <form action="">
+                  <Modal.Header closeButton>
+                    <Modal.Title>Write Your Review</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body className="modals-body">
+                    {LoggeduserData.map((userdata, index) => {
+                      return (
                         <>
-                        <div key={index}>
-                           <label htmlFor="">User Name</label>
-                             <input type="text" value={userdata.firstname || ""} 
-                             placeholder="username" 
-                             name="username"  
-                             disabled
-                             />
-                         </div>
+                          <div key={index}>
+                            <label htmlFor="">User Name</label>
+                            <input
+                              type="text"
+                              value={userdata.firstname || ""}
+                              placeholder="username"
+                              name="username"
+                              disabled
+                            />
+                          </div>
                         </>
-                      )
-                    })
-                  }
-                  <div className="mt-3">
-                    <label htmlFor="">Rateings</label>
-                    <Select options={options} onChange={(e)=>handleselect(e)}/>
-                  </div>
-                  <div className="mt-3">
-                    <textarea
-                      className="text-area"
-                      name="description"
-                      id=""
-                      placeholder="Write here"
-                      onChange={(e)=>setDesc(e.target.value)}
-                    ></textarea>
-                  </div>
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="success" onClick={submitReview}>
-                    Add Review
-                  </Button>
-                  
-                </Modal.Footer>
+                      );
+                    })}
+                    <div className="mt-3">
+                      <label htmlFor="">Rateings</label>
+                      <Select
+                        options={options}
+                        onChange={(e) => handleselect(e)}
+                      />
+                    </div>
+                    <div className="mt-3">
+                      <textarea
+                        className="text-area"
+                        name="description"
+                        id=""
+                        placeholder="Write here"
+                        onChange={(e) => setDesc(e.target.value)}
+                      ></textarea>
+                    </div>
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button variant="success" onClick={submitReview}>
+                      Add Review
+                    </Button>
+                  </Modal.Footer>
                 </form>
-
               </Modal>
             </div>
           </div>
           <div className="d-flex gap-3 flex-wrap justify-content-center mt-3">
-              {
-                getReview_state?.map((item,index)=>{
-                  return(
-                    <>
-                      <div>
-                      
-              <Card style={{ width: "18rem", height:"100%" }}>
-                <Card.Body>
-                  <Card.Title>{item.username}</Card.Title>
-                  <Card.Text>
+            {getReview_state.length > 0 ? (
+              getReview_state?.map((item, index) => {
+                return (
+                  <>
                     <div>
-                         <p>{item.description}</p>
+                      <Card style={{ width: "18rem", height: "100%" }}>
+                        <Card.Body>
+                          <Card.Title>{item.username}</Card.Title>
+                          <Card.Text>
+                            <div>
+                              <p>{item.description}</p>
+                            </div>
+                            <p>
+                              {/* For showing rateing stars */}
+                              <div className="product-details__ratings">
+                                <span
+                                  className="product-details__stars"
+                                  style={{ color: "orange" }}
+                                >
+                                  {Array.from({ length: item.rating }).map(
+                                    (ele, ind) => {
+                                      return <i class="fa-solid fa-star"></i>;
+                                    }
+                                  )}
+                                  {/* Empty Stars */}
+                                  {Array.from({ length: 5 - item.rating }).map(
+                                    (_, index) => (
+                                      <i
+                                        key={index + item.rating}
+                                        className="fa-regular fa-star"
+                                      ></i>
+                                    )
+                                  )}
+                                </span>
+                                <span
+                                  className="product-details__rating-count"
+                                  style={{ marginLeft: "5px" }}
+                                ></span>
+                              </div>
+                            </p>
+                          </Card.Text>
+                          {LoggeduserData[0]._id === item.userid ? (
+                            <Button
+                              variant="none"
+                              onClick={() => deleteReview(item._id)}
+                            >
+                              <i
+                                className="fa-solid fa-trash"
+                                style={{ color: "red", fontSize: "22px" }}
+                              ></i>
+                            </Button>
+                          ) : (
+                            ""
+                          )}
+                        </Card.Body>
+                      </Card>
                     </div>
-                    <p>
-                      {/* For showing rateing stars */}
-                      <div className="product-details__ratings">
-                        <span
-                          className="product-details__stars"
-                          style={{ color: "orange" }}
-                        >
-                          {
-                             Array.from({ length: item.rating }).map((ele, ind) => {
-                                   return <i class="fa-solid fa-star"></i>
-                                })
-                          }
-                          {/* Empty Stars */}
-                           {Array.from({ length: 5 - item.rating }).map((_, index) => (
-                           <i key={index + item.rating} className="fa-regular fa-star"></i>
-                            ))}
-                        </span>
-                        <span
-                          className="product-details__rating-count"
-                          style={{ marginLeft: "5px" }}
-                        >
-                         
-                        </span>
-                      </div>
-                    </p>
-                  </Card.Text>
-                  {
-                    LoggeduserData.length > 0 ?
-                  <Button variant="none" onClick={()=>deleteReview(item._id)}>
+                  </>
+                );
+              })
+            ) : (
+              <div className="no-reviews">
+                <div className="d-flex justify-content-center gap-3">
+                  <div>
                     <i
-                      className="fa-solid fa-trash"
-                      style={{ color: "red", fontSize: "22px" }}
+                      className="fa-solid fa-comment-dots"
+                      style={{ fontSize: "50px", color: "#07c3de" }}
                     ></i>
-                  </Button> : ""
-                  }
-                </Card.Body>
-              </Card>
-            </div>
-                      </>
-                    )
-                })
-              }
+                  </div>
+                  <div>
+                    <h4
+                      className="text-center mt-2"
+                      style={{ color: "#555", fontWeight: "bold" }}
+                    >
+                      No Reviews Yet!
+                    </h4>
+                  </div>
+                </div>
+                <p className="text-muted">
+                  Be the first to share your experience. Click the button above
+                  to write a review!
+                </p>
+              </div>
+            )}
           </div>
         </Container>
       </section>
