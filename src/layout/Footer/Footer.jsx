f37b46b283b0544fd85de5c1ff1f1c908c082893
Dinesh sharma <dineshsharmadev93@gmail.com>
2025-03-21 16:42:07 +0530

    work on shipping page

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
-
+import './footer.scss'
 export default function Footer() {
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className='mt-5'>
+      <footer class="footer">
+  <div class="footer-container">
+    <div class="about footer-section">
+      <h3>About Us</h3>
+      <p>We provide the best services for online recharges and payments.</p>
+    </div>
+    <div class="footer-section links">
+      <h3>Quick Links</h3>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+    <div class="footer-section links">
+      <h3>Quick Links</h3>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Pricing</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+    <div class="footer-section social">
+      <h3>Follow Us</h3>
+      <div class="social-icons">
+        <a href="#"><i class="fa-facebook fab"></i></a>
+        <a href="#"><i class="fa-twitter fab"></i></a>
+        <a href="#"><i class="fa-instagram fab"></i></a>
+        <a href="#"><i class="fa-linkedin fab"></i></a>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2025 YourCompany. All rights reserved.</p>
+  </div>
+</footer>
+
     </div>
   )
 }
